c9fbf51c9b03ccc010c78f60a51e6108b03be122
Response051 <platoon1277@gmail.com>
2023-05-23 14:48:46 +0100

    ALL BASIC Styling were made(using tailwind framework.)

--- Birthday-project/birthdayslist/src/App.js
+++ Birthday-project/birthdayslist/src/App.js
@@ -9,10 +9,19 @@ function App() {
     setfirst([]);
   }
   return (
-    <div>
-      <p>{first.length} Birthdays Remaining</p>
-      <List persons={first} />
-      <button onClick={handleClear}>Clear Birthdays</button>
+    <div className="bg-pink-600">
+      <p>Heloo world</p>
+      <nav className="bg-white text-stone-950 my-10 mx-96 pl-6 pt-4 pb-5 border-2">
+        <p className="mb-6 text-2xl">{first.length} Birthdays Remaining</p>
+        <List persons={first} />
+        <button
+          onClick={handleClear}
+          className="bg-pink-600 text-white rounded-none mt-6 px-14 py-1"
+        >
+          Clear Birthdays
+        </button>
+      </nav>
+      <p>Heloo world</p>
     </div>
   );
 }
--- Birthday-project/birthdayslist/src/List.js
+++ Birthday-project/birthdayslist/src/List.js
@@ -4,12 +4,16 @@ function List({ persons }) {
   return (
     <div>
       {persons.map((person) => {
-        const { id, name, email, gender, image } = person;
+        const { id, Name, email, gender, image } = person;
         return (
-          <article key={id}>
-            <img src={image} alt="name" className="img" />
-            <div>
-              <p>{name}</p>
+          <article key={id} className="flex flex-row ">
+            <img
+              src={image}
+              alt="name"
+              className="img my-2 w-6/12 sm:w-4/12 px-4 shadow rounded-full max-w-full h-auto align-middle border-none"
+            />
+            <div className="flex-space">
+              <p className="text-stone-950 font-bold">{Name}</p>
               <p>{email}</p>
               <p>{gender}</p>
             </div>
